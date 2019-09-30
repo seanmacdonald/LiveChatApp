576f58de246309e8d1e968f923aed4c339531877
Sean MacDonald <seandoylemacdonald@gmail.com>
2019-09-30 13:57:12 -0700

    made endpoint for getting list of chats and created function for making a new chat

--- main/main.go
+++ main/main.go
@@ -4,20 +4,44 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"io"
+	"bytes"
 
 	"github.com/gorilla/websocket"
 )
 
+//TODO: For now store users and chats in global variables but 
+//		should move them to a db later. 
 var users = make([]string, 0)
+var chats = make(map[string][]*websocket.Conn)
+
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func getChats(w http.ResponseWriter, r *http.Request) {
+	if r.Method !=  "GET" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		io.WriteString(w, "Bad request.")
+		return 
+	} 
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	b := new(bytes.Buffer)
+	for key, _ := range chats {
+		fmt.Fprintf(b, "%s\n", key)
+	}
+	
+	io.WriteString(w, b.String())
+	
 }
 
 func connect(w http.ResponseWriter, r *http.Request) {
-	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
-
 	//Firt get the username from the request
 	if err := r.ParseForm(); err != nil {
 		fmt.Println(err)
@@ -32,12 +56,14 @@ func connect(w http.ResponseWriter, r *http.Request) {
 	}
 
 	//Now that we have the user we can setup the websocket connection
-	_, err := upgrader.Upgrade(w, r, nil)
+	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	log.Println("Server has a new connection to user named:", user)
+
+	handleChat(conn)
 
 }
 
@@ -46,9 +72,25 @@ func addUser(user string) {
 	log.Println("Current users:", users)
 }
 
+func addChat(chat_name string, conn *websocket.Conn) {
+	if _, exists := chats[chat_name]; exists {
+		log.Println("Chat name alreay exists...")
+		return
+	}
+
+	conns := make([]*websocket.Conn, 1)
+	conns[0] = conn
+	chats[chat_name] = conns
+}
+
+func handleChat(conn *websocket.Conn) {
+
+}
+
 func main() {
 	fmt.Println("Starting LiveChatApp...")
 
 	http.HandleFunc("/connect", connect)
+	http.HandleFunc("/chats", getChats)
 	http.ListenAndServe(":8080", nil)
 }
