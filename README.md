a2100bf5ac7a5f41a55abf4174971c540fc15f20
Sean MacDonald <seandoylemacdonald@gmail.com>
2019-10-04 14:08:34 -0700

    modified the message protocol to use coded numbers instead of number of colons and updated readme for the messaging protocol

--- README.md
+++ README.md
@@ -22,10 +22,18 @@ Connect a new user to the server using a websocket.
 **URL Params (required)** - user=[username]  
 
 ### Messaging Protocol
-The protocol used between the client and server through the websocket connection.  
+The protocol used between the client and server through the websocket connection.
+
+
 **Client to Server** - Send message to the server with the format:  
-*chat_name:message*   
-Where chat_name is the name of the chat that the user wants to send a message to and message is the message they want to send. Note that the client should ensure that there are not colons used in the chat_name. However, there may be colons in the message.   
+There are 4 types of messages the client can send the server.  
+- Case 1: Broadcast message in form: 1<chatname>:<username>:<msg> 
+- Case 2: Make new chat in form: 2<chatname>  
+- Case 3: Join existing chat in form: 3<chatname>  
+- Case 4: Delete existing chat in form: 4<chatname>    
+
+Where 'chatname' is the name of the chat that the user wants to send a message to and 'msg' is the message they want to send. Note that the client should ensure that there are not colons used in the chatname nor the username. However, there may be colons in the msg.  
+ 
 **Server to Client** - The server will send a message to all users part of the chat in the form:   
 *chat_name: user_name: message*   
 The client is responsible for parsing the chat_name and possibly user_name for displaying the information however it would like.    
--- handler/msgh.go
+++ handler/msgh.go
@@ -4,40 +4,40 @@ import (
 	"log"
 	"strings"
 	"fmt"
+	"strconv"
 
 	"github.com/gorilla/websocket"
 	"github.com/seanmacdonald/LiveChatApp/data"
 )
 
 //Figures out what the incoming message is for. There are 4 cases: 
-//	1: Broadcast message in form <chat_name>:<user_name>:<msg>
-//	2: Make new chat in form :<chat_name>
-//	3: Join existing chat in form ::<chat_name>
-//	4: Delete existing chat in form :::<chat_name>  
+//	1: Broadcast message in form 1<chat_name>:<user_name>:<msg>
+//	2: Make new chat in form 2<chat_name>
+//	3: Join existing chat in form 3<chat_name>
+//	4: Delete existing chat in form 4<chat_name>  
 //NOTE: a chat name CANNONT be an empty string. 
 func HandleMessage(user string, msg string, conn *websocket.Conn, chat_info *data.ChatData) {
-	//first figure out how many semicolons are in a row 
-	count := 0 
-	for _, char := range msg {
-		if char != ':' {
-			break
-		}
-		count++
+	//get first character of msg which is the case identifier 
+	var count int
+	if val, err := strconv.ParseInt(string(msg[0]), 10, 0); err == nil {
+		count = int(val)
 	}
+
+	parsedMsg := msg[1:]
 
 	//note that count corresponds to the index where chat_name starts in the message 
 	switch count {
-	case 0: 
-		log.Println("Broadcast case")
-		broadcastMessage(user, msg, chat_info)
 	case 1: 
-		log.Println("Make new chat case")
-		createChat(count, user, msg, conn, chat_info)
+		log.Println("Broadcast message")
+		broadcastMessage(user, parsedMsg, chat_info)
 	case 2: 
-		log.Println("Join existing chat case")
+		log.Println("Make new chat:", parsedMsg)
+		createChat(user, parsedMsg, conn, chat_info)
 	case 3: 
-		log.Println("Leave existing chat case")
-		leaveChat(count, user, msg, conn, chat_info)
+		log.Println("Join existing chat:", parsedMsg)
+	case 4: 
+		log.Println("Leave existing chat:", parsedMsg)
+		leaveChat(user, parsedMsg, conn, chat_info)
 	default: 
 		log.Println("Error parsing message")
 	}
@@ -69,12 +69,12 @@ func broadcastMessage(user string, msg string, chat_info *data.ChatData) {
 	}
 }
 
-func createChat(pos int, user string, msg string, conn *websocket.Conn, chat_info *data.ChatData) {
-	chat_name := msg[pos:]
-	data.AddChatGroup(chat_name, conn, chat_info)
+func createChat(user string, msg string, conn *websocket.Conn, chat_info *data.ChatData) {
+	//msg is the chat name 
+	data.AddChatGroup(msg, conn, chat_info)
 }
 
-func leaveChat(pos int, user string, msg string, conn *websocket.Conn, chat_info *data.ChatData) {
-	chat_name := msg[pos:]
-	data.LeaveChatGroup(chat_name, conn, chat_info)
+func leaveChat(user string, msg string, conn *websocket.Conn, chat_info *data.ChatData) {
+	//msg is the chat name
+	data.LeaveChatGroup(msg, conn, chat_info)
 }
